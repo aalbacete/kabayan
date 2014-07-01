8a4e2f74660e335dec6d3641d98a6debf46bf7c6
aalbacete <allan.albacete@gmail.com>
2014-07-01 14:13:05 +0800

    .

--- Kabayan.DataAccess/WebsiteCms.cs
+++ Kabayan.DataAccess/WebsiteCms.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kabayan.Domain.Context;
+using Kabayan.Domain.Migrations;
 using Kabayan.Domain.Models;
-
+using Kabayan.Domain.Common;
 namespace Kabayan.DataAccess
 { 
     public class WebsiteCmsDataAccess
     {
+        
         
 
         public WebsiteCmsDataAccess()
@@ -21,13 +23,25 @@ namespace Kabayan.DataAccess
         public List<MessageLog> MessageLogs {
             get
             {
-                using (var kabayanDb = new KabayanContext())
+                try
                 {
-                    var res = (from a in kabayanDb.MessageLogs
-                        select a).ToList();
+                    using (var kabayanDb = new KabayanContext())
+                    {
+                        var res = (from a in kabayanDb.MessageLogs
+                                   select a).ToList();
 
-                    return res;
+                        return res;
+                    }
                 }
+                catch(Exception ex)
+                {
+
+                    if (KabayanCollection.DebugMode)
+                    {
+                        //var l = new SystemLogs{ ApplicationName = "Test"} ;
+                    }  else throw new Exception(ex.Message);                         
+                }
+               return null;
             }
         }
         
